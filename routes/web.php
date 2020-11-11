a0f203d9304938154df00bd3555f7084d8bb9eb7
‘Andrei <andrei.dima@usm.ro>
2020-11-11 16:45:10 +0200

    'a'

--- routes/web.php
+++ routes/web.php
@@ -44,8 +44,52 @@ Route::get('/teste-modale', [App\Http\Controllers\TesteController::class, 'teste
 Route::post('/teste-modale-apasa-buton', [App\Http\Controllers\TesteController::class, 'testeModaleApasaButon']);
 Route::post('/teste-modale-apasa-buton-2', [App\Http\Controllers\TesteController::class, 'testeModaleApasaButon2']);
 Route::get('/test', function () {
-    // dd(App\Models\Factura::select('seria')->latest()->first());
-    return App\Models\Factura::select('seria')->latest()->first()->seria ?? 'lol';
+
+    // $context  = stream_context_create(array('https' => array('header' => 'Accept: application/xml')));
+    // // $url = 'https://www.bnr.ro/nbrfxrates.xml ';
+    // $url = 'https://bjvrancea.ro/temp/nbrfxrates.xml';
+
+    // $xml = file_get_contents($url, false, $context);
+    // $xml = simplexml_load_string($xml);
+
+
+    // $xml1=simplexml_load_file("https://bjvrancea.ro/temp/books.xml") or die("Error: Cannot create object");
+    $xml=simplexml_load_file("https://bjvrancea.ro/temp/nbrfxrates.xml") or die("Error: Cannot create object");
+    // echo $xml->book[0]->title . "<br>";
+    // echo $xml->book[1]->title;
+    
+    $curs_bnr_euro = 0;
+    foreach($xml->Body->Cube->children() as $curs_bnr) {
+        if ((string) $curs_bnr['currency'] === 'EUR'){
+            $curs_bnr_euro = $curs_bnr[0];
+        }
+    }
+    echo $curs_bnr_euro;
+
+    // echo $curs_bnr['currency'];
+    // echo $books[0];
+    // print_r($books);
+    // echo "<br>";
+    // echo $curs_bnr['currency'];
+    // echo $books[0];
+    // echo "<br><br>";
+    // print_r($books['currency']);
+    // echo "<br>";
+    // print_r($books[0]);
+    // echo "<br>";
+    // print_r($books->attributes->currency[0]);
+    // echo "<br>";
+    // print_r($books['@attributes']);
+    // echo "<br>";
+    // echo $books->attributes . ", ";
+    // echo $books->Rate . ", ";
+    // echo $books->year . ", ";
+    // echo $books->price . "<br><br>";
+
+    // dd($xml, $xml1, $xml1->book, $xml2->Body->Cube->Rate);
+    // print_r($xml, $xml['SimpleXMLElement']['cube']['rate']['10']);
+    // print_r($xml['Subject']);
+    
 });
 
 Route::group(['middleware' => 'auth'], function () {
