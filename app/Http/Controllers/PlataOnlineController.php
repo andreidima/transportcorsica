b8f63fd475c53fa32e5f3025daff3b0f9377c43e
‘Andrei <andrei.dima@usm.ro>
2020-12-16 12:17:42 +0200

    'a'

--- app/Http/Controllers/PlataOnlineController.php
+++ app/Http/Controllers/PlataOnlineController.php
@@ -84,11 +84,11 @@ class PlataOnlineController extends Controller
              */
             $this->billingAddress = new Address();
             $this->billingAddress->type         = "persoana"; //should be "person" / "company"
-            $this->billingAddress->firstName    = "Billing LastName" ?? $rezervare_tur->pasageri_relation->first()->nume ?? 'Nedefinit';
+            $this->billingAddress->firstName    = "Billing FirstName" ?? $rezervare_tur->pasageri_relation->first()->nume ?? 'Nedefinit';
             $this->billingAddress->lastName     = "Billing LastName";
-            $this->billingAddress->address      = "Billing LastName" ?? $rezervare_tur->oras_plecare_nume->oras ?? 'Nedefinit';
-            $this->billingAddress->email        = "Billing LastName" ?? $rezervare_tur->email ?? 'Nedefinit';
-            $this->billingAddress->mobilePhone  = "Billing LastName" ?? $rezervare_tur->telefon ?? 'Nedefinit';
+            $this->billingAddress->address      = "Billing Address" ?? $rezervare_tur->oras_plecare_nume->oras ?? 'Nedefinit';
+            $this->billingAddress->email        = "Billing Email" ?? $rezervare_tur->email ?? 'Nedefinit';
+            $this->billingAddress->mobilePhone  = "Billing mobilePhone" ?? $rezervare_tur->telefon ?? 'Nedefinit';
             $paymentRequest->invoice->setBillingAddress($this->billingAddress);
 
             /*
@@ -96,11 +96,11 @@ class PlataOnlineController extends Controller
              */
             $this->shippingAddress = new Address();
             $this->shippingAddress->type        = "persoana"; //should be "person" / "company"
-            $this->shippingAddress->firstName   = "Billing LastName" ?? $rezervare_tur->pasageri_relation->first()->nume ?? 'Nedefinit';
+            $this->shippingAddress->firstName   = "Shipping FirstName" ?? $rezervare_tur->pasageri_relation->first()->nume ?? 'Nedefinit';
             $this->shippingAddress->lastName    = "Shipping LastName";
-            $this->shippingAddress->address     = "Billing LastName" ?? $rezervare_tur->oras_plecare_nume->oras ?? 'Nedefinit';
-            $this->shippingAddress->email       = "Billing LastName" ?? $rezervare_tur->email ?? 'Nedefinit';
-            $this->shippingAddress->mobilePhone = "Billing LastName" ?? $rezervare_tur->telefon ?? 'Nedefinit';
+            $this->shippingAddress->address     = "Shipping Address" ?? $rezervare_tur->oras_plecare_nume->oras ?? 'Nedefinit';
+            $this->shippingAddress->email       = "Shipping Email" ?? $rezervare_tur->email ?? 'Nedefinit';
+            $this->shippingAddress->mobilePhone = "Shipping mobilePhone" ?? $rezervare_tur->telefon ?? 'Nedefinit';
             $paymentRequest->invoice->setShippingAddress($this->shippingAddress);
 
             /*
