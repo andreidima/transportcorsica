b38bdea8fc014fdc623b2aaf6f8367c9575d88b7
‘Andrei <andrei.dima@usm.ro>
2021-01-27 12:29:12 +0200

    'a'

--- resources/views/chitante/export/chitanta.blade.php
+++ resources/views/chitante/export/chitanta.blade.php
@@ -82,7 +82,7 @@
                
             <br><br>          
                             
-            {{-- <table style="margin-bottom:0px">
+            <table style="margin-bottom:0px">
                 <tr style="text-align:center; font-weight:bold;">
                     <td colspan="3" style="border-width:0px; padding:0rem;">
                         <h3 style="background-color:#e7d790; color:black; margin:0px 0px 5px 0px; padding:5px 0px;">
@@ -120,7 +120,7 @@
    
             <br><br>
 
-            <table style="margin-bottom:0px">    
+            {{-- <table style="margin-bottom:0px">    
                 <tr style="text-align:center; font-weight:bold;">
                     <td colspan="4" style="padding:0rem;">
                         <h3 style="background-color:#e7d790; color:black; margin:10px 0px 5px 0px; padding:5px 0px">
@@ -224,8 +224,8 @@
             </table>
             
             <br><br>
-            Păstrați biletul pentru control --}}
-            
+            Păstrați biletul pentru control
+             --}}
              
         </div>
     </div>
