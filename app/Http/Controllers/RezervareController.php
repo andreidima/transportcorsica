669b507f948ee8a271c58c052ce0ba1549c89786
‘Andrei <andrei.dima@usm.ro>
2021-02-08 11:00:13 +0200

    Imprimanta de bilete

--- app/Http/Controllers/RezervareController.php
+++ app/Http/Controllers/RezervareController.php
@@ -1376,8 +1376,8 @@ class RezervareController extends Controller
             return view('chitante.export.chitanta', compact('rezervare'));
         } elseif ($request->view_type === 'export-pdf') {
                 $pdf = \PDF::loadView('chitante.export.chitanta', compact('rezervare'))
-                    // ->setPaper([0,0,420,1000]);
-                    ->setPaper('a5', 'portrait');
+                    ->setPaper([0,0,420,1000]);
+                    // ->setPaper('a5', 'portrait');
                 return $pdf->stream();
                 // return $pdf->download('Chitanta.pdf');
         }
