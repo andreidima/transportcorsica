eca833fcdd0a1a943de2512ba93b50b230f5dc13
‘Andrei <andrei.dima@usm.ro>
2021-02-09 10:31:48 +0200

    Imprimanta de bilete

--- resources/views/chitante/export/chitanta.blade.php
+++ resources/views/chitante/export/chitanta.blade.php
@@ -223,16 +223,21 @@
                     @endif   
             </table>   
             
+            @php
+                $html = 'asdasdasdasdasd';
+            @endphp
+
             <br>
             Păstrați biletul pentru control
             <br>
             <a class="btn btn-primary btn-cta" onclick="return checkAndroid();" 
-                href="intent://rezervari.transportcorsica.ro/chitanta-descarca/tur5ff97b920cbb8/export-html
+                href="intent:{{$html}}
                     #Intent;scheme=rawbt;package=ru.a402d.rawbtprinter;end;">
                 {{-- <svg class="svg-inline--fa fa-print fa-w-16" aria-hidden="true" data-prefix="fas" data-icon="print" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg="">
                     <path fill="currentColor" d="M464 192h-16V81.941a24 24 0 0 0-7.029-16.97L383.029 7.029A24 24 0 0 0 366.059 0H88C74.745 0 64 10.745 64 24v168H48c-26.51 0-48 21.49-48 48v132c0 6.627 5.373 12 12 12h52v104c0 13.255 10.745 24 24 24h336c13.255 0 24-10.745 24-24V384h52c6.627 0 12-5.373 12-12V240c0-26.51-21.49-48-48-48zm-80 256H128v-96h256v96zM128 224V64h192v40c0 13.2 10.8 24 24 24h40v96H128zm304 72c-13.254 0-24-10.746-24-24s10.746-24 24-24 24 10.746 24 24-10.746 24-24 24z" data-darkreader-inline-fill="" style="--darkreader-inline-fill:currentColor;">
                     </path></svg> --}}
-                    <!-- <i class="fas fa-print"></i> --> Print Now
+                    <!-- <i class="fas fa-print"></i> --> 
+                    Print Now
             </a>
             {{-- <a href="print://escpos.org/escpos/bt/print?srcTp=uri
    &srcObj=html
