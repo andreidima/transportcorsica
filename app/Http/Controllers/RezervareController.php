0924137f08eb9fce8c651269c22251c25dc85e22
‘Andrei <andrei.dima@usm.ro>
2021-02-26 12:01:26 +0200

    a

--- app/Http/Controllers/RezervareController.php
+++ app/Http/Controllers/RezervareController.php
@@ -425,7 +425,7 @@ class RezervareController extends Controller
                     $rezervare_tur->delete();
                 }
             }
-//1gdds
+//1gddssd
             return back()->with('status', 'Rezervarea a fost ștearsă cu succes!');
         } else {
             return back()->with('error', 'Rezervarea nu poate fi ștearsă pentru că are deja bilet emis!');
