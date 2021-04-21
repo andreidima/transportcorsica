9dcdc277bcff3a2f86d1b5d687dc1501054c6e27
unknown <andrei.dima@usm.ro>
2021-04-21 07:11:11 +0300

    a

--- resources/views/chitante/export/seteaza-orase.blade.php
+++ resources/views/chitante/export/seteaza-orase.blade.php
@@ -10,7 +10,13 @@
                         <img src="{{ asset('images/logo.png') }}" height="100" class="">
                     </div>
                     <div class="col-lg-12 mb-0 d-flex justify-content-center">
-                        <h1 class="mb-0" style="color:darkcyan">CHITANȚĂ</h1>
+                        <h1 class="mb-0" style="color:darkcyan">
+                            @isset($rezervare->nr_adulti)
+                                CHITANȚĂ
+                            @else
+                                AWB
+                            @endisset
+                        </h1>
                     </div>
                 </div>
 
@@ -32,53 +38,69 @@
                             <div class="form-group col-lg-12 px-2 mb-0">
 
                                 <div class="form-row mb-4 d-flex justify-content-center">
-                                    <div class="col-lg-12 mb-5 h5">
-                                        Chitanță pentru biletul pasagerilor:
-                                        @isset($rezervare->nr_adulti)
+                                    @isset($rezervare->nr_adulti)
+                                        <div class="col-lg-12 mb-5 h5">
+                                            Chitanță pentru biletul pasagerilor:
                                             @foreach ($rezervare->pasageri_relation as $pasager)
                                                 {{ $pasager->nume }}{{ (!$loop->last) ? ', ' : '.'}}
                                             @endforeach
-                                        @else
-                                            Rezervare colete
-                                        @endif
-                                    </div>
-                                    <div class="col-lg-2 mb-4">
-                                        <label for="oras_plecare" class="mb-1 h5">Oraș plecare:*</label>
-                                        <input
-                                            type="text"
-                                            class="form-control form-control-lg {{ $errors->has('oras_plecare') ? 'is-invalid' : '' }}"
-                                            name="oras_plecare"
-                                            placeholder=""
-                                            value="{{ old('oras_plecare', ($rezervare->oras_plecare_nume->oras ?? '')) }}"
-                                            required>
-                                    </div>
-                                    <div class="col-lg-2 mb-5">
-                                        <label for="oras_sosire" class="mb-1 h5">Oraș sosire:*</label>
-                                        <input
-                                            type="text"
-                                            class="form-control form-control-lg {{ $errors->has('oras_sosire') ? 'is-invalid' : '' }}"
-                                            name="oras_sosire"
-                                            placeholder=""
-                                            value="{{ old('oras_sosire', ($rezervare->oras_sosire_nume->oras ?? '')) }}"
-                                            required>
-                                    </div>
+                                        </div>
+                                    @else
+                                        <div class="col-lg-12 mb-4 h5 text-center">
+                                            <span class="px-2 border" style="font-size: 1.5em; background-color:lightseagreen; color:white">
+                                                {{ $rezervare->oras_plecare_nume->oras }} -> {{ $rezervare->oras_sosire_nume->oras }}
+                                            </span>
+                                            <br>
+                                            <br>
+                                            <span class="px-2 border" style="font-size: 1.5em; background-color:lightseagreen; color:white">
+                                                {{ $rezervare->colete_numar }} colete
+                                            </span>
+                                        </div>
+                                    @endif
+
 
                                     @isset($rezervare->nr_adulti)
+                                        <div class="col-lg-2 mb-4">
+                                            <label for="oras_plecare" class="mb-1 h5">Oraș plecare:*</label>
+                                            <input
+                                                type="text"
+                                                class="form-control form-control-lg {{ $errors->has('oras_plecare') ? 'is-invalid' : '' }}"
+                                                name="oras_plecare"
+                                                placeholder=""
+                                                value="{{ old('oras_plecare', ($rezervare->oras_plecare_nume->oras ?? '')) }}"
+                                                required>
+                                        </div>
+                                        <div class="col-lg-2 mb-5">
+                                            <label for="oras_sosire" class="mb-1 h5">Oraș sosire:*</label>
+                                            <input
+                                                type="text"
+                                                class="form-control form-control-lg {{ $errors->has('oras_sosire') ? 'is-invalid' : '' }}"
+                                                name="oras_sosire"
+                                                placeholder=""
+                                                value="{{ old('oras_sosire', ($rezervare->oras_sosire_nume->oras ?? '')) }}"
+                                                required>
+                                        </div>
                                     @else
-                                    <div class="col-lg-12 mb-5">
-                                        <label for="pret_total" class="mb-1 h5">Preț:*</label>
-                                        <input
-                                            type="text"
-                                            class="form-control form-control-lg {{ $errors->has('pret_total') ? 'is-invalid' : '' }}"
-                                            name="pret_total"
-                                            placeholder=""
-                                            value="{{ old('pret_total', ($rezervare->pret_total ?? '')) }}"
-                                            required>
-                                    </div>
+                                        <div class="col-lg-2 mb-5">
+                                            <label for="pret_total" class="mb-1 h5">Preț:*</label>
+                                            <input
+                                                type="text"
+                                                class="form-control form-control-lg {{ $errors->has('pret_total') ? 'is-invalid' : '' }}"
+                                                name="pret_total"
+                                                placeholder=""
+                                                value="{{ old('pret_total', ($rezervare->pret_total ?? '')) }}"
+                                                required>
+                                        </div>
                                     @endisset
 
                                     <div class="col-lg-12 d-flex justify-content-center">
-                                        <button type="submit" class="btn btn-lg btn-primary border border-white btn-block">EMITE CHITANȚA</button>
+                                        <button type="submit" class="btn btn-lg btn-primary border border-white btn-block">
+                                            @isset($rezervare->nr_adulti)
+                                                EMITE CHITANȚA
+                                            @else
+                                                EMITE AWB
+                                            @endif
+                                        </button>
                                     </div>
                                 </div>
                             </div>
--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,10 @@ Route::get('/adauga-rezervare-pasul-3', [RezervareController::class, 'adaugaReze
 
 Route::get('/bilet-rezervat/{view_type}', [RezervareController::class, 'pdfExportGuest']);
 Route::get('/factura-descarca/{view_type}', [RezervareController::class, 'exportPDFGuest']);
+
+// Imprimanta termica
+// 1. Setare orase si emitere bilet pasageri
+// 2. Setare kilograme si emitere awb colete
 Route::get('/chitanta-descarca/{cheie_unica}/seteaza_orase', [RezervareController::class, 'chitantaSeteazaOraseGuest']);
 Route::post('/chitanta-descarca/{cheie_unica}/seteaza_orase', [RezervareController::class, 'postChitantaSeteazaOraseGuest']);
 Route::get('/chitanta-descarca/{cheie_unica}/{view_type}', [RezervareController::class, 'chitantaExportPDFGuest'])->name('chitanta-descarca');
