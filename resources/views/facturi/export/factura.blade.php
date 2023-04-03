2280d43e99189dc8ee6eef07bf1dd7dc8c19f905
Andrei Dima <andrei.dima@usm.ro>
2023-04-03 21:07:35 +0300

    a

--- resources/views/facturi/export/factura.blade.php
+++ resources/views/facturi/export/factura.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Factura</title>
     <style>
-        body { 
+        body {
             font-family: DejaVu Sans, sans-serif;
             /* font-family: Arial, Helvetica, sans-serif;  */
             font-size: 10px;
@@ -24,26 +24,26 @@
             margin-top: 0px;
             border-style: solid;
             border-width:0px;
-            width: 100%; 
+            width: 100%;
             word-wrap:break-word;
             /* word-break: break-all; */
             /* table-layout: fixed; */
         }
-        
+
         th, td {
             padding: 5px 5px;
             border-width:1px;
             border-style: solid;
             table-layout:fixed;
             font-weight: normal;
-            
+
         }
         tr {
             /* text-align:; */
             /* border-style: solid;
             border-width:1px; */
         }
-        hr { 
+        hr {
             display: block;
             margin-top: 0.5em;
             margin-bottom: 0.5em;
@@ -51,14 +51,14 @@
             margin-right: auto;
             border-style: inset;
             border-width: 0.5px;
-        } 
+        }
     </style>
 </head>
 
 <body>
         <div style="
             border:dashed #999;
-            width:680px;         
+            width:680px;
             padding: 10px 15px 10px 15px;
             margin:0px 0px;
                 -moz-border-radius: 10px;
@@ -66,13 +66,13 @@
                 border-radius: 10px;
                 ">
 
-                      
+
             <table style="">
                 <tr style="">
                     <td style="border-width:0px; padding:0rem; width:50%; vertical-align:top;">
                             <p style="background-color:teal; color: white; padding:5px; width:50%">
-                                <b>FURNIZOR</b> 
-                            </p>                     
+                                <b>FURNIZOR</b>
+                            </p>
                             <b>MRW88 MAXARMONY S.R.L.</b> <br>
                             Nr. ord. reg. com./an: J39/570/29.09.2015 <br>
                             CIF: RO35059906 <br>
@@ -86,8 +86,8 @@
                     </td>
                     <td style="border-width:0px; padding:0rem; width:50%; vertical-align:top;">
                             <p style="background-color:teal; color: white; padding:5px; width:50%">
-                                <b>CUMPĂRĂTOR</b> 
-                            </p>     
+                                <b>CUMPĂRĂTOR</b>
+                            </p>
                             <b>{{ $factura->cumparator }}</b> <br>
                             Nr. Reg. com.: {{ $factura->nr_reg_com }} <br>
                             CIF: {{ $factura->cif }} <br>
@@ -96,26 +96,26 @@
                 </tr>
             </table>
         </div>
-        
+
         <div style="
-            width:680px;         
+            width:680px;
             padding: 10px 0px 10px 15px;
             margin:0px 0px;
                 -moz-border-radius: 10px;
                 -webkit-border-radius: 10px;
                 border-radius: 10px;">
-            
-            
+
+
             <h2 style="text-align: center; color:teal; margin-bottom:0px">
-                FACTURĂ 
+                FACTURĂ
                 {{-- {{($factura->anulare_factura_id_originala !== null) ? 'ANULATĂ' : ''}} --}}
             </h2>
             <p style="text-align: center; margin-top:0px">
                 (de decont)
             </p>
 
-            <p style="text-align: center; ">                        
-                        Seria și număr: <b>{{ $factura->seria }} {{ $factura->numar }}</b> 
+            <p style="text-align: center; ">
+                        Seria și număr: <b>{{ $factura->seria }} {{ $factura->numar }}</b>
                         {{-- <br>
                         Nr.: <b>{{ $factura->numar }}</b> --}}
                         <br>
@@ -140,14 +140,14 @@
             @endif
         </div>
 
-            
+
                     <table style="width:100%;">
                         <tr style="background-color:teal; color:white;">
                             <th style="text-align: center; padding:2px">DESCRIERE</th>
                             <th style="text-align: center; padding:2px">CANT.</th>
                             <th style="text-align: center; padding:2px">PREȚ UNITAR</th>
                             <th style="text-align: center; padding:2px">SUMĂ</th>
-                        </tr> 
+                        </tr>
                         <tr style="height:200px; vertical-align:top;">
                             <td>
                                 Servicii Transport Internațional Persoane <br><br>
@@ -165,10 +165,10 @@
                                         </li>
                                     </ul>
                                 @else
-                                    Bilet de călătorie: {{ $factura->rezervare->bilet_serie ?? '' }} {{ $factura->rezervare->bilet_numar ?? '' }} | Dată transport: {{ \Carbon\Carbon::parse($factura->rezervare->data_cursa ?? '')->isoFormat('DD.MM.YYYY') }}. <br>                                
+                                    Bilet de călătorie: {{ $factura->rezervare->bilet_serie ?? '' }} {{ $factura->rezervare->bilet_numar ?? '' }} | Dată transport: {{ \Carbon\Carbon::parse($factura->rezervare->data_cursa ?? '')->isoFormat('DD.MM.YYYY') }}. <br>
                                 @endisset
                                 <br>
-                                Pasageri:                                
+                                Pasageri:
                                     @isset($factura->rezervare->nr_adulti)
                                         @foreach ($factura->rezervare->pasageri_relation as $pasager)
                                             @if(!$loop->last)
@@ -179,18 +179,18 @@
                                         @endforeach
                                     @endisset
                                 <br><br>
-                                Preț în EURO: {{ round($factura->valoare_euro) }}&euro; 
+                                Preț în EURO: {{ round($factura->valoare_euro) }}&euro;
                                 <br><br>
                                 Curs valutar BNR la data rezervării biletului de călătorie: {{ \Carbon\Carbon::parse($factura->rezervare->created_at)->isoFormat('DD.MM.YYYY') }}: <br>
-                                1 EURO = {{ $factura->curs_bnr_euro }} 
+                                1 EURO = {{ $factura->curs_bnr_euro }}
                             </td>
                             <td style="text-align:center">
                                 {{($factura->anulare_factura_id_originala !== null) ? '-' : ''}}1
                             </td>
                             <td style="text-align:right">
                                 {{
-                                    ($factura->anulare_factura_id_originala !== null) ? 
-                                        (-1 * ($factura->valoare_lei + $factura->valoare_lei_tva)) 
+                                    ($factura->anulare_factura_id_originala !== null) ?
+                                        (-1 * ($factura->valoare_lei + $factura->valoare_lei_tva))
                                         : ($factura->valoare_lei + $factura->valoare_lei_tva)
                                 }}
                             </td>
@@ -225,22 +225,21 @@
                         @endif --}}
                         </tr>
                     </table>
-        
+
         <br>
         <br>
 
         <p style="text-align:center">
-            Întocmit de: Manolache Viorel, CNP 1781113390675, seria și nr. buletin VN 478829.
+            Întocmit de: Manolache Viorel, CNP 1781113390675, seria și nr. buletin VN 855400.
         </p>
 
         <p style="text-align:center">
-            Dacă aveți întrebări legate de această factură, trimiteți un email la: 
+            Dacă aveți întrebări legate de această factură, trimiteți un email la:
             <br>
             <b>rezervari@transportcorsica.ro</b>
         </p>
-        
- 
+
+
 </body>
 
 </html>
-    
